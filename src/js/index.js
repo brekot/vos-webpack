d35f7d685b823ea845c3577353dbf90bec19a5fb
Andrey Brekotkin <brekot@ya.ru>
2020-06-01 00:34:51 +0300

    Две страницы полностью

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,20 @@ import Swiper from './swiper.js';
 window.jQuery = $;
 window.$ = $;
 
+import a from './jquery.mousewheel.js';
+import b from './jquery.jscrollpane.min.js';
+
 $(function() {
+
+    $('.menu-top-btn').click(function(){
+
+        $('.menu-top').toggleClass('menu-top_active');
+    });
+
+    $('.menu-top__close').click(function(){
+
+        $('.menu-top').removeClass('menu-top_active');
+    });
 
     new Swiper('.slider-main__slider', {
         loop: true,
@@ -17,4 +30,15 @@ $(function() {
             bulletActiveClass: 'slider-main__point_active',
         },
     });
+
+    if ($(window).width() > 1360)
+    {
+        var scrollPane = $('.scroll-pane__block').jScrollPane();
+        var scrollApi = scrollPane.data('jsp');
+
+        $('.scroll-pane__down').click(function(){
+
+            scrollApi.scrollByY(50);
+        });
+    }
 });
